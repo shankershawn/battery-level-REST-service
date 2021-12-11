84d7dfe2ece5d63fa3d9570e8bf2e0097af12856
Shankarsan Ganai <shankershawn@gmail.com>
2021-12-11 20:35:11 +0530

    checkin

--- src/main/java/com/shankarsan/auto/battery/dto/BaseResponseDTO.java
+++ src/main/java/com/shankarsan/auto/battery/dto/BaseResponseDTO.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class BaseResponseDTO {
 	
+	@JsonProperty("status")
 	private String errorCode;
 	@JsonProperty("message")
 	private String errorMessage;
--- src/main/java/com/shankarsan/auto/battery/enums/ErrorMessageEnum.java
+++ src/main/java/com/shankarsan/auto/battery/enums/ErrorMessageEnum.java
@@ -2,7 +2,7 @@ package com.shankarsan.auto.battery.enums;
 
 public enum ErrorMessageEnum {
 	
-	SUCCESS("","Successfully processed."),
+	SUCCESS("OK","Successfully processed."),
 	INVALID_INPUT("ERR-03","Invalid input provided."),
 	SERVICE_DOWN("ERR-002", "Service is temporarily down. Please try again later."),
 	NO_DATA_RECEIVED("ERR-001", "No data recieved from database");
